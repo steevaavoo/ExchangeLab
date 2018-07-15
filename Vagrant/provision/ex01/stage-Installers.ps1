10e218bc0c352695dcee83137620d9e8ae8ab252
Steve Baker <steevaavoo@gmail.com>
2018-07-15 15:24:31 +0100

    Added -Force to Exchange source file copy

--- Vagrant/provision/ex01/stage-Installers.ps1
+++ Vagrant/provision/ex01/stage-Installers.ps1
@@ -1,4 +1,4 @@
 # Self-explanatory. .\vagrant folder can be unreliable - safer to copy to C:\ and run scripts from there
 Write-Host 'Copying Exchange Installation files to C:\Source - this will take about 5 minutes - 5.6GB!'
-Copy-Item "C:\Vagrant\Source" -Destination "C:\" -Recurse
+Copy-Item "C:\Vagrant\Source" -Destination "C:\" -Recurse -Force
 Write-Host 'Done!'
